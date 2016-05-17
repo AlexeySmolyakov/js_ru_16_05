c06767400177741c7edfe99c54a1192cb53a4557
Alexey Smolyakov <smolyakov.alexey@gmail.com>
2016-05-18 00:09:15 +0300

    First lesson.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,33 @@
-console.log('---', 'hello world')
+import React from 'react'
+import { render } from 'react-dom'
+import ArticleList from './components/ArticleList.js'
+
+const articles = [
+	{
+		id: 1,
+		title: 'Article title',
+		text: 'Lorem ipsum dolor sit amet'
+	},
+	{
+		id: 2,
+		title: 'Second article title',
+		text: 'Lorem ipsum dolor sit amet'
+	},
+	{
+		id: 3,
+		title: 'Third article title',
+		text: 'Lorem ipsum dolor sit amet'
+	},
+	{
+		id: 4,
+		title: 'Another article title',
+		text: 'Lorem ipsum dolor sit amet'
+	},
+	{
+		id: 5,
+		title: 'More info title',
+		text: 'Lorem ipsum dolor sit amet'
+	}
+];
+
+render(<ArticleList articles={articles}/>, document.getElementById('container'));
---
+++ src/components/Article.js
@@ -0,0 +1,38 @@
+import React, {PropTypes, Component} from 'react'
+
+class Article extends Component {
+	state = {
+		isOpen: false
+	}
+
+	render () {
+		const {article} = this.props;
+		if (!article) return <h3>No article!</h3>;
+		const {title, text, id} = article;
+
+		const { isOpen } = this.state;
+		const textItem = isOpen ? <section>{text}</section> : null;
+
+		return (
+			<div>
+				<h3 onClick={ this.handleClick }>{title}</h3>
+				{textItem}
+			</div>
+		)
+	}
+
+	handleClick = (ev) => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+}
+
+Article.propTypes = {
+	article: PropTypes.shape({
+		title: PropTypes.string,
+		text: PropTypes.string.isRequired
+	}).isRequired
+};
+
+export default Article;
